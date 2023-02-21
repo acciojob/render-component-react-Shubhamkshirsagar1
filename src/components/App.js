e2b404f8cf93ed5043aea6d58b7ab0bf5c0c1bce
shubham kshirsagar <117492782+Shubhamkshirsagar1@users.noreply.github.com>
2023-02-21 10:55:18 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,11 @@ const App = () => {
   return (
     <div id="main">
       <p>I am learning React. My life is getting better.</p>
+
+      <h1 data-ns-test='project-name' id='project-name'> Digital Vehicle Fuel Level Indicator </h1>
+
+      <p data-ns-test='project-description' id='project-description'> In our project the main blocks are micro controller unit, fuel level sensor and LCD display unit. The fuel level detection circuit is used to detect the level of the fuel in the tank; here sensors are placed at certain places to find out the fuel level and the signals are sent to the micro controller unit for further operations. </p>
+      
     </div>
   )
 }
